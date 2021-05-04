855789c10867c87782d8c62f147af45efeffa678
Dilan Patel <dpatel173@hawk.iit.edu>
2021-05-04 17:27:32 -0500

    project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,2 +1,47 @@
-def radix_a_book(book-url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+import urllib.request
+
+def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
+    book = urllib.request.urlopen(book_url).read().decode()
+    words = book.split()
+    maxLen = len(max(words, key=len))
+    return radix(maxLen, words)
+
+def radix(maxLen, words):
+    zeroToMax = range(0, maxLen)
+    #We dont know character codes for evreything in the book
+    #We can append to buckets as needed
+    buckets = [] 
+    for i in zeroToMax.__reversed__():
+        for string in words:
+            charCode = None
+            if i < len(string):
+                charCode = ord(string[i]) 
+            else:
+                #always make this one first
+                charCode = 0
+            #add to buckets until we support the new character
+            while len(buckets) <= charCode:
+                buckets.append([])
+            buckets[charCode].append(string) 
+            
+        words = []
+        for bucket in buckets:
+            words += bucket
+            del bucket[:]
+    return words
+
+
+# Test code
+
+# def sortTest(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
+#     book = urllib.request.urlopen(book_url).read().decode()
+#     words = book.split()
+#     words.sort()
+#     return words
+
+# print(radix_a_book())
+# print(sortTest())
+
+# print(radix_a_book() == sortTest())
+
+
